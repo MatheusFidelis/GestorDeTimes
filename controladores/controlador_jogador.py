edd42043eaac1388d8eef164d980d0f5b34a5ae3
MatheusFidelis <matheusfidelis2@gmail.com>
2022-10-31 02:02:37 -0300

    Funcao alterar jogador finalizada

--- controladores/controlador_jogador.py
+++ controladores/controlador_jogador.py
@@ -33,12 +33,30 @@ class ControladorJogador(ControladorAbstrato):
 
             contrato = Contrato(dados["contrato"]["salario"], dados["contrato"]["multa"]) #objeto contrato
 
-            jogador = Jogador(dados["nome"], int(dados["idade"]), dados["posicao"], int(
-                dados["camisa"]), contrato)
+            jogador = Jogador(dados["nome"], int(dados["camisa"]), dados["posicao"], int(
+                dados["idade"]), contrato)
 
             self.__jogadores.append(jogador)
             self.__tela.mensagem("cadastro realizada com sucesso")
             break
+
+    def novos_dados(self):
+        while True:
+            dados = self.__tela.dados_jogador()
+            if not self.verificar_dados(dados):
+                continue
+            for jogador in self.__jogadores:
+                if jogador.camisa == int(dados["camisa"]):
+                    self.__tela.mensagem_erro("número da camisa existente")
+                    continue
+
+            #contrato = Contrato(dados["contrato"]["salario"], dados["contrato"]["multa"]) #objeto contrato
+
+            #jogador = Jogador(dados["nome"], int(dados["idade"]), dados["posicao"], int(
+            #    dados["camisa"]), contrato)
+
+
+            return dados
 
     def verificar_dados(self, dados):
 
@@ -59,38 +77,62 @@ class ControladorJogador(ControladorAbstrato):
 
         return True
 
-    def dados_jogador(self, jogador):
+    def dados_jogador(self, jogador): # transforma os dados em dicionario
         dados = {"nome": jogador.nome, "idade": jogador.idade, "posicao": jogador.posicao,
                  "camisa": jogador.camisa, "salario": jogador.contrato.salario, "multa": jogador.contrato.multa}
 
         return dados
 
     def seleciona_jogador(self):
-        camisa = self.__tela.seleciona()
-        for jogador in self.__jogadores:
-            if jogador.camisa == int(camisa):
-                return jogador
-        return False
-
+        valid = False
+        while not valid:
+            try:
+                camisa = int(self.__tela.seleciona())
+            except ValueError:
+                self.__tela.mensagem_erro("Por favor digite um valor numérico inteiro")
+            else:
+                valid = True
+        return camisa
 
     def alterar(self):
-        jogador = self.seleciona_jogador()
-        if isinstance(jogador, Jogador):
-            self.__tela.mostra_jogador(self.dados_jogador(jogador))
-            while True:
-                dados = self.__tela.dados_alterar()
-                dados["camisa"] = Jogador.camisa
-                if not self.verificar_dados(dados):
-                    continue
-                jogador.nome = dados["nome"]
-                jogador.idade = dados["idade"]
-                jogador.posicao = dados["posicao"]
-                jogador.camisa = dados["camisa"]
+        camisa = self.seleciona_jogador()
 
-                self.__tela.mensagem("Alteração realizada com sucesso")
-                break
-        else:
+        i = 0
+        encontrado = False
+        while i < len(self.__jogadores) and (not encontrado):
+            print(self.__jogadores[i].camisa)
+            if self.__jogadores[i].camisa == camisa:
+                encontrado = True
+                posicao = i
+            i += 1
+
+        if not encontrado:
             self.__tela.mensagem_erro("Jogador não encontrado")
+            return
+
+        dados = self.novos_dados()
+        print(dados)
+        contrato = Contrato(dados["contrato"]["salario"], dados["contrato"]["multa"])  # objeto contrato
+
+        try:
+            jogador = Jogador(dados["nome"], (dados["camisa"]), dados["posicao"], int(dados["idade"]), contrato)
+
+            self.__jogadores.pop(posicao)
+            self.__jogadores.append(jogador)
+            self.__tela.mensagem("Alteração realizada com sucesso")
+
+            #self.__jogadores[posicao].nome = dados["nome"]
+            #self.__jogadores[posicao].idade = dados["idade"]
+            #self.__jogadores[posicao].posicao = dados["posicao"]
+            #elf.__jogadores[posicao].camisa = dados["camisa"]
+            #elf.__jogadores[posicao].contrato.salario = dados['contrato']['salario']
+            #self.__jogadores[posicao].contrato.multa = dados['contrato']['multa']
+        except:
+            print("Erro ao tentar alterar")
+
+
+        print(self.__jogadores)
+
 
     def remover(self): #aqui vai a rescisao
         self.__tela.mensagem("=== REMOVER JOGADOR ===")
@@ -104,14 +146,14 @@ class ControladorJogador(ControladorAbstrato):
         encontrado = False
         try:
             while (i < len(self.__jogadores)) and (not encontrado):
-                if self.__jogadores[i].camisa == camisa:
+                if self.__jogadores[i].camisa == camisa and self.__tela.confirma_exclusao():
                     self.__jogadores.pop(i)
                     encontrado = True
                     self.__tela.mensagem("Jogador removido com sucesso")
                 return
                 i += 1
         except:
-            self.__tela.mensagem_erro("Jogador não encontrado")
+            self.__tela.mensagem_erro("Jogador não existe")
 
         else:
             self.__tela.mensagem("Jogador removido com sucesso")
